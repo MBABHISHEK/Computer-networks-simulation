c397c2cb51ac10505bcdbcd56753d83ddfd16cdd
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-12-27 22:06:01 +0530

    Create link.tcl

---
+++ link.tcl
@@ -0,0 +1,11 @@
+
+
+
+$ns_ at 1.0 "$node_(4) add-mark m blue square"
+$ns_ at 1.0 "$node_(20) add-mark m magenta square"
+
+$ns_ at 1.0 "$node_(4) label SENDER"
+$ns_ at 1.0 "$node_(20) label RECEIVER"
+
+
+$ns_ at 0.01 "$ns_ trace-annotate \"Network Deployment\""
